99a122189d1233a3d8bf4fb3aca643014040b6c0
Magistry <Magistrigusti@yandex.ru>
2023-06-18 08:53:14 +0300

    0.3

--- src/components/FeatureOne.jsx
+++ src/components/FeatureOne.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import TokenCard from './TokenCard';
 
 const FeatureOne = () => {
-  const tokens = ['0px','gosh-realm']
+  const tokens = ["0x","gosh-realm"]
 
-  return (
+  return (  
     <div className='feature-container'>
       <div className='swap-token-container'>
         <div className='tokens-container'>
           {
             tokens.map((token, index) => {
-              <>{token}</>
+              return (
+                <TokenCard id={token} />
+              )
             })
           }
         </div>
--- src/components/TokenCard.jsx
+++ src/components/TokenCard.jsx
@@ -1,10 +1,49 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const TokenCard = () => {
+const TokenCard = ({ id }) => {
   const [tokenData, setTokenData] = useState(null);
-  
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const apiUrl = `https://api.coingecko.com/api/v3/coins/${id}`;
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setTokenData(data);
+        setIsLoading(false);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+
   return (
-    <div>TokenCard</div>
+    <div className="token-container">
+      {tokenData && (
+        <>
+          <div className="token-info">
+            <img className="token-logo" src={tokenData.image.small} alt='' />
+            <div className="token-title-container">
+              <span className="token-title">{tokenData.name}</span>
+              <span className="token-title-long">
+                {tokenData.asset_platform_id}
+              </span>
+            </div>
+          </div>
+          <div className="token-amount-container">
+            <div className="token-amount">10</div>
+            <div className="token-amount-long">
+              <div className="token-price">
+                {tokenData.market_data.current_price.usd}
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
   );
 };
 
