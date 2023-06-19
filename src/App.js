a63800dca2aeaa171e29fd871547214f88c7d096
Magistry <Magistrigusti@yandex.ru>
2023-06-19 09:19:01 +0300

    0.3.1a

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import React from 'react';
-import Header from './components/Header';
-import HeroSection from './components/HeroSection';
-import FeatureOne from './components/FeatureOne';
+
 import './App.css';
 
 function App() {
   return (
     <div className="home">
-      <Header />
-      <HeroSection />
-      <FeatureOne />
+
     </div>
   );
 }
