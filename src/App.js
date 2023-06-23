d6b71228a0ddf9e33b03bd171c20be0ab4716dcc
Magistry <Magistrigusti@yandex.ru>
2023-06-23 08:05:08 +0300

    0.4.1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 function App() {
-  let likes = 0;
+  const [likes, setLikes] = useState(0);
+  
+  function increment() {
+    setLikes( likes + 1 );
+  }
+
+  function decrement() {
+    setLikes( likes - 1 );
+  }
+  
   return (
     <div className="App">
-    <h1>{likes}</h1>
-      <button onClick={() => likes += 1}>Increment</button>
-      <button onClick={() => likes -= 1}>Decrement</button>
+      <h1>{likes}</h1>
+      <button oClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </div>
   );
 }
