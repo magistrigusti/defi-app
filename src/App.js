30297c51bbc1f61ab245c463bb88d5b4bc273329
Magistry <Magistrigusti@yandex.ru>
2023-06-25 06:00:33 +0300

    0.4.3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Counter from './components/Counter';
 import ClassCounter from './components/ClassCounter';
+import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
+import MyInput from './components/UI/input/MyInput';
 import './styles/App.css';
 
 
 function App() {
-const [value, setValue] = useState('Kniaz');
+const [posts, setPosts] = useState([
+  {id: 1, title: 'JavaScript', body: 'Description'},
+  {id: 2, title: 'React', body: 'Description'},
+  {id: 3, title: 'Redux', body: 'Description'},
+]);
+const [title, setTitle] = useState('Kniaz');
+const [body, setBody] = useState('Krasava');
+
+const addNewPost = (event) => {
+  event.preventDefault();
+}
   
   return (
     <div className="App">
+      <form>
+        <MyInput onChange={event => setTitle(event.target.value)} value={title} />
 
+        <MyInput onChange={event => setBody(event.target.value)} value={body} />
+        
+        <MyButton onClick={addNewPost}>Add Post</MyButton>
+      </form>
+      
+      <PostList posts={posts} title={'Post List for JS'} />
     </div>
   );
 }
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const PostItem = (props) => {
+  return (
+    <div className="post">
+      <div className="post__container">
+        <strong>{props.post.id} {props.post.title}</strong>
+
+        <div>{props.post.body}</div>
+
+        <div className="post-btns">
+          <button>Delete</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PostItem;
