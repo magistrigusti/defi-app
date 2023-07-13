7ee180b95bddaca354ae1c99b2dfaeffb27fd25a
Magistry <Magistrigusti@yandex.ru>
2023-07-13 08:19:17 +0300

    0.6

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Routes , Route, Link } from 'react-router-dom';
-import About from './components/pages/About';
-import Posts from './components/pages/Posts';
-
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/UI/Navbar/Navbar';
+import AppRouter from './components/AppRouter';
 import './styles/App.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="navbar">
-        <div className="navbar__links">
-          <Link to="/about">About as</Link>
-          <Link to="/posts">Posts</Link>
-        </div>
-      </div>
-      <Routes>
-        <Route exact path="/about" element={<About/>}/>
-
-        <Route exact path="/posts" element={<Posts/>}/>
-      </Routes>
+        <Navbar />
+        <AppRouter />
     </BrowserRouter>
   )
 }
